633fe9c2cd29bcc8dae621a45e270c38cbbf3b10
PSum <sum.philipp@gmail.com>
2024-05-20 17:53:23 +0200

    update
    styling updates

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,5 +1,10 @@
 import './styles/mainStyles.css'
+import { Link } from 'react-router-dom'
 
 export default function Homepage (){
-    return <div className='Homepage'>This is the homepage text</div>
+    return <div className='Homepage'><h2>Welcome to the homepage of the Shopping-Cart-Project of the odin project curriculum.</h2>
+   <div>
+   Jump to the <Link to='shop'>Shoppage</Link> to start shopping
+    </div> 
+    </div>
 }
